9a518696fc48c7c720bfb430bff813f5dd661c75
Stavros Ntioudis <42137665+sntioudis@users.noreply.github.com>
2024-04-10 13:37:32 +0100

    Update test_brownian.sh
    Change python script name to updated name
    
    Signed-off-by: Stavros Ntioudis <42137665+sntioudis@users.noreply.github.com>

--- Examples/Brownian Self Diffusion/test_brownian.sh
+++ Examples/Brownian Self Diffusion/test_brownian.sh
@@ -10,6 +10,6 @@ mv ./*.log ./results
 
 #Run dedicated python script to plot distributions (change the input to the python script if you want to plot other distributions from the distributions folder).
 #A distributions.jpg file will be generated in the parent directory with the requested plot
-python3 plot_bins.py ./results 10 4000 10000 20000 100000
+python3 plot_distributions.py ./results 10 4000 10000 20000 100000
 
 #Remember to delete the results folder if you wish to rerun
