89adce7d955c37fd591915e9fd4e251333044c9f
Stavros Ntioudis <42137665+sntioudis@users.noreply.github.com>
2024-05-23 11:39:43 +0100

    Update run_tests.sh
    Signed-off-by: Stavros Ntioudis <42137665+sntioudis@users.noreply.github.com>

--- tests/source tests/run_tests.sh
+++ tests/source tests/run_tests.sh
@@ -1,1 +1,4 @@
-mpiexec ./build/source_tests -in ./input_files/in_kmc.lmp ./input_files/in_kmc.ppc
+#!/bin/bash
+paprecatests_dir=$1 #Retrieve path from script invocation
+
+mpiexec "${paprecatests_dir}/source_tests" -in ./input_files/in_kmc.lmp ./input_files/in_kmc.ppc
