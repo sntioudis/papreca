0a8b3dc46291c85979aaa62fa54222b94badf697
Stavros Ntioudis <42137665+sntioudis@users.noreply.github.com>
2024-09-28 12:29:02 +0300

    Allowed negative diffusion distances.
    For film growth cases where the film can grow towards negative z-coordinates.
    
    Signed-off-by: Stavros Ntioudis <42137665+sntioudis@users.noreply.github.com>

--- source/libraries/PAPRECA/input_file.cpp
+++ source/libraries/PAPRECA/input_file.cpp
@@ -847,9 +847,6 @@ namespace PAPRECA{
 		
 		double velocity = string2Double( commands[2] );
 		if( velocity < 0 ){ allAbortWithMessage( MPI_COMM_WORLD , "velocity in " + commands[0] + " command has to be non-negative."); }
-		
-		double distance = string2Double( commands[3] );
-		if( distance < 0 ){ allAbortWithMessage( MPI_COMM_WORLD , "diffusion_distance in " + commands[0] + " command has to be non-negative."); }
 				
 		bool is_displacive = string2Bool( commands[4] );
 		
