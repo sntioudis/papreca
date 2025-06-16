4ebf1882aa5246c361a85f69f4e8b5ef8ca4a128
Stavros Ntioudis <42137665+sntioudis@users.noreply.github.com>
2025-06-16 17:03:13 +0100

    Update build_LAMMPS.sh
    Update to ensure compatibility with new LAMMPS tag (12June2025)
    
    Signed-off-by: Stavros Ntioudis <42137665+sntioudis@users.noreply.github.com>

--- Installation/autobuild/build_LAMMPS.sh
+++ Installation/autobuild/build_LAMMPS.sh
@@ -9,7 +9,7 @@ cd ../../
 mkdir build; cd build
 
 #Pull LAMMPS from LAMMPS repository
-git clone --depth 1 --branch patch_29Aug2024 https://github.com/lammps/lammps.git LAMMPS
+git clone --depth 1 --branch patch_12June2025 https://github.com/lammps/lammps.git LAMMPS
 
 #Copy source files for fix PAPRECA
 cd LAMMPS
